029185ef267c69aa2601fc2825d16051c4b60b9e
harry moreno <morenoh149@gmail.com>
2012-02-15 17:42:54 -0500

    comments

--- DecisionTree/src/DTLearning.java
+++ DecisionTree/src/DTLearning.java
@@ -1,4 +1,10 @@
+import java.util.List;
 
+/**
+ * decision tree learning takes place here,
+ * @author harry
+ *
+ */
 public class DTLearning extends Node{
 	Node root;
 	
--- DecisionTree/src/Solution.java
+++ DecisionTree/src/Solution.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
 
+/**
+ * our solution requires the data to be provided in a csv file,
+ * this is common practice in Machine Learning so we used the opencsv library available
+ * for java
+ * NOTE: no attempt is made to parse the values, we treat all attributes as strings,
+ * this allows us to handle any type of dataset
+ * @author harry moreno
+ * @author ben leone
+ */
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
