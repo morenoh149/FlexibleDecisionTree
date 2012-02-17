8e5e2788302ed884558ad1edcc2b2a6848a6048f
ben <ben@Phobos-ubuntu>
2012-02-16 20:27:13 -0500

    stuff

--- DecisionTree/src/DTLearning.java
+++ DecisionTree/src/DTLearning.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +38,73 @@ public class DTLearning extends Node{
 	 * given a dataset and an attribute index, calculate the entropy value of splitting
 	 * on that attribute
 	 */
+	
+	private int entropy(int attribute, List<String[]> dataset){
+		int result = 0;
+		List<Integer> results = new ArrayList<Integer>(0);
+		List<List<String[]>> sorted = new ArrayList<List<String[]>>(0);
+		List<String> category = new ArrayList<String>(0);
+		int total = dataset.size(); 
+		String temp = null; //holds test stirngs
+		int count = 0; //holds indexes
+		boolean contanined = false;
+		for(String[] select : dataset){ //go through all string arrays
+			temp = select[attribute];   // get the attribute we are working on
+			if(category.size()==0){     // check if the attribute holder is empty to add stuff
+				category.add(select[attribute]);      //adds our atrbuite to the log
+				sorted.add(new ArrayList<String[]>(0)); //initialize storage for string arrays
+				sorted.get(1).add(select);           // add our string to the stored set by similar attribute
+			}else{                       //the attribute holder is not empty so see if our attribute is already logged
+				contanined=false;
+				count = 1;
+				for(String check : category){
+					if(check.equals(temp)){
+						sorted.get(count).add(select);
+						contanined = true;
+					}
+					count++;
+				}
+				if(contanined==false){
+					category.add(select[attribute]);
+					sorted.add(new ArrayList<String[]>(0));
+					sorted.get(count).add(select);
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public double importance(List<String[]> dataset){
+		int total = dataset.size();
+		int end;
+		int count;
+		boolean found;
+		List<Integer> results = new ArrayList<Integer>(0);
+		List<String> category = new ArrayList<String>(0);
+		for(String[] select : dataset){
+			end = select.length-1;
+			if(category.size()==0){
+				category.add(select[end]);
+				results.add(1);
+			}else{
+				found=false;
+				count = 1;
+				for(String temp : category){
+					if(temp.equals(select[end])){
+						results.set(count, results.get(count)+1);
+						found=true;
+					}
+				}
+				if(found==false){
+					results.add(1);
+					category.add(select[end]);
+				}
+			}
+		}
+		return 0.0;
+	}
+	
 //	int entropy(int attribute, List<String[]> dataset){
 //		Map<String, Integer> map = new HashMap<String, Integer>();
 //		String[] firstrow = dataset.get(0);
@@ -61,19 +129,19 @@ public class DTLearning extends Node{
 	/**
 	 * given a dataset, return the index of the most important attribute
 	 */
-	int importance(List<String[]> dataset){
-		String[] firstrow = dataset.get(0);
-		//index, entropy value
-		HashMap<Integer, Integer> importanceValues = new HashMap<Integer, Integer>();
-		for(int i=0; i<firstrow.length; i++){
-//			importanceValues.put(i, entropy(i,dataset));
-		}
-		int max = 0;
-		for(int i=0; i<importanceValues.size(); i++){
-			if(importanceValues.get(i)>max)
-				max = i;
-		}
-		return max;
+//	int importance(List<String[]> dataset){
+//		String[] firstrow = dataset.get(0);
+//		//index, entropy value
+//		HashMap<Integer, Integer> importanceValues = new HashMap<Integer, Integer>();
+//		for(int i=0; i<firstrow.length; i++){
+////			importanceValues.put(i, entropy(i,dataset));
+//		}
+//		int max = 0;
+//		for(int i=0; i<importanceValues.size(); i++){
+//			if(importanceValues.get(i)>max)
+//				max = i;
+//		}
+//		return max;
 	}
 
 	/**
